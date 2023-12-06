5487f8c40f5d628d40ec41af01b1f74fca7ed21b
TeddyAdams <80841726+IrmaAdams@users.noreply.github.com>
2023-12-06 13:48:32 +0000

    Logger Class

--- Irma Prototype/PrototypeCOM646/src/Game.cpp
+++ Irma Prototype/PrototypeCOM646/src/Game.cpp
@@ -1,4 +1,5 @@
 #include "Game.h"
+#include "Logger.h"
 #include <iostream>
 #include <SDL_image.h>
 #include <glm/glm.hpp>
@@ -6,15 +7,15 @@
 
 Game::Game() {
 	isRunning = false;
-	std::cout << "Game constructor is called" << std::endl;
+	Logger::Log("Game constructor called");
 }
 Game::~Game() {
-	std::cout << "Game destructor is called" << std::endl;
+	Logger::Log("Game destructor is called");
 }
 
 void Game::Initialize() {
 	if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
-		std::cerr << "Error initializing SDL" << std::endl;
+		Logger::Err("Error initializing SDL");
 		return;
 	}
 
@@ -32,13 +33,13 @@ void Game::Initialize() {
 		);
 
 	if (!window) {
-		std::cerr << "Error creating SDL window" << std::endl;
+		Logger::Err("Error creating SDL window");
 		return;
 	}
 
 	renderer = SDL_CreateRenderer(window, -1, 0);
 	if (!renderer) {
-		std::cerr << "Error creating SDL renderer" << std::endl;
+		Logger::Err("Error creating SDL renderer");
 		return;
 	}
 	SDL_SetWindowFullscreen(window, SDL_WINDOW_FULLSCREEN);			// change the video mode to fullscreen
